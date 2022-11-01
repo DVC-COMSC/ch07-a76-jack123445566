0b5609a8a64b5d70fc15922d7e2cd24d6662e0b5
jack123445566 <112512759+jack123445566@users.noreply.github.com>
2022-11-01 20:35:22 +0000

    2nd commit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 
 num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
-
-num3 = num1+num2
-
+if len(num1) < len(num2):
+    num3 = num1+num2
+else:
+    num3 = num2+num1
 print(num3) 
